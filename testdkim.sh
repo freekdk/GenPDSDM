45ed1f2eb12c9467c89a963d9b6b79d58d288b4a
Freek de Kruijf <freekdekruijf@kde.nl>
2024-06-11 18:09:26 +0200

    testdkim replaced by script using s-nail

--- testdkim.sh
+++ testdkim.sh
@@ -1,30 +1,21 @@
 #!/bin/bash
 # insert DKIM test email message in submission port 587
-# after that inspect the recieved message for presence of a DKIM item in the header
 #
-if [ ! -x /usr/bin/expect ] ; then
-    grep opensuse /etc/os-release
-    if [ $? -eq 0 ] ; then
-	sudo zypper in expect
-    else
-	apt install expect
-    fi
-fi
-echo "Script to test generation of DKIM item in outgoing email via submission port 587"
-echo "It needs the domain name you used to generate the DKIM certificate,"
-echo "the user name of a user on this system and its password. You will be prompted"
-echo "for these values. The password will not be visible. The destination of the"
-echo "message is the user where messages for root are directed to."
-echo "The first paramter \"$1\" replaces localhost:587 by the value the connection goes to."
-echo "The second parameter \"$2\" replaces the To email address root@localhost.<domain>"
-dest_port="localhost:587"
-[ "$1" != "" ] && dest_port="$1"
-echo ""
-echo "==================="
-echo ""
-echo -n "Enter the domain name: "
-read domain
-echo ""
+echo "Script to test an outgoing email via submission port 587 to specified server"
+echo "The following items will be asked for:"
+echo "The email from address, the email destination address, the server,"
+echo "the user name of a user on this server and its password."
+echo
+echo -n "From address: "
+read from
+echo
+echo -n "Destination address: "
+read dest
+echo
+echo -n "Name of server: "
+read server
+dest_port="$server:587"
+echo
 echo -n "Enter the user name: "
 read username
 echo
@@ -39,17 +30,14 @@ do
     prompt='*'
     password+="$char"
 done
-echo ""
-dest="root@localhost.$domain"
-[ "$2" != "" ] && dest="$2"
-authbase64=$(echo -en "\0$username\0$password" | base64)
-date=$(LC_ALL=C date --date=now "+%a, %d %b %Y %H:%M:%S %z")
-echo testje: ${0%/*} $(ls ${0%/*}/testdkim.*)
-${0%/*}/testdkim.exp "$dest_port" "root@$domain" "$dest" "$authbase64" "$date"
+echo
+echo "mailx -v -S smtp-use-starttls -S smtp=$dest_port -S smtp-auth=plain -S smtp-auth-password=$password -S smtp-auth-user=$username -S ssl-ca-file=/etc/postfix/ssl/cacert.pem -s testmessage -r $from $dest <<EOF"
+mailx -v -S smtp-use-starttls -S smtp=$dest_port -S smtp-auth=plain -S smtp-auth-password=$password -S smtp-auth-user=$username -S ssl-ca-file=/etc/postfix/ssl/cacert.pem -S ssl-verify=ignore -s testmessage -r $from $dest <<EOF
+Test message
+EOF
 if [ $? -eq 0 ] ; then
     echo ""
     echo "Sending test message to $dest via ${dest_port%:*} succeeded."
-    echo "Now look for the message in the folder ~/Maildir/new/ of the user, email for root is directed to."
     echo ""
     exit 1
 else
